a086c24eb2648f5fb8b3c6b0a7f580e53db0d8e2
Colton Acosta <cacost12@asu.edu>
2022-06-24 20:32:59 -0700

    added implementation of continuity functions

--- terminal/rev4/mod/ignition/ignition.c
+++ terminal/rev4/mod/ignition/ignition.c
@@ -66,7 +66,20 @@ bool solid_prop_cont
 	void
 	)
 {
-return false;
+
+/* Check MCU GPIO State */
+uint8_t solid_prop_cont_pinstate = HAL_GPIO_ReadPin(SP_CONT_GPIO_PORT, SP_CONT_PIN);
+
+/* Return true if GPIO state is high*/
+if (solid_prop_cont_pinstate == 0)
+	{
+    return true;
+	}
+else
+	{
+    return false;
+    }
+
 } /* solid_prop_cont */
 
 
@@ -85,7 +98,20 @@ bool nozzle_cont
 	void
 	)
 {
-return false;
+
+/* Check MCU GPIO State */
+uint8_t nozzle_cont_pinstate = HAL_GPIO_ReadPin(NOZ_CONT_GPIO_PORT, NOZ_CONT_PIN);
+
+/* Return true if GPIO state is high*/
+if (nozzle_cont_pinstate == 0)
+	{
+    return true;
+	}
+else
+	{
+    return false;
+    }
+
 } /* nozzle_cont */
 
 
@@ -104,7 +130,19 @@ bool ematch_cont
 	void
 	)
 {
-return false;
+/* Check MCU GPIO State */
+uint8_t ematch_cont_pinstate = HAL_GPIO_ReadPin(E_CONT_GPIO_PORT, E_CONT_PIN);
+
+/* Return true if GPIO state is high*/
+if (ematch_cont_pinstate == 0)
+	{
+    return true;
+	}
+else
+	{
+    return false;
+    }
+
 } /* ematch_cont */
 
 
