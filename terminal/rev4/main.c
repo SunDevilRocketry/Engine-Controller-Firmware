16b114b437470638ed50efc3d6b2ed0323405d6f
Colton Acosta <cacost12@asu.edu>
2022-07-08 18:29:00 -0700

    added flash SPI initializations

--- terminal/rev4/main.c
+++ terminal/rev4/main.c
@@ -38,9 +38,10 @@
 
 
 /*------------------------------------------------------------------------------
- MCU Peripheral Handlers                                                                  
+ MCU Peripheral Handlers                                                              
 ------------------------------------------------------------------------------*/
 UART_HandleTypeDef huart1; /* USB UART handler struct */
+SPI_HandleTypeDef  hspi2;  /* Flash SPI handle */
 
 
 /*------------------------------------------------------------------------------
@@ -48,7 +49,8 @@ UART_HandleTypeDef huart1; /* USB UART handler struct */
 ------------------------------------------------------------------------------*/
 static void	SystemClock_Config(void); /* clock configuration */
 static void GPIO_Init(void); /* GPIO configurations  */
-static void USB_UART_Init(void); /* USB UART configuration */
+static void USB_UART_Init(void); /* USB UART configuration  */
+static void MX_SPI2_Init(void);  /* Flash SPI configuration */
 
 
 /*------------------------------------------------------------------------------
@@ -77,6 +79,7 @@ HAL_Init();           /* Reset peripherals, initialize flash interface and
 SystemClock_Config(); /* System clock                                         */
 GPIO_Init();          /* GPIO                                                 */
 USB_UART_Init();      /* USB UART                                             */
+MX_SPI2_Init();
 
 /*------------------------------------------------------------------------------
  Event Loop                                                                  
@@ -191,13 +194,13 @@ RCC_OscInitStruct.HSEState = RCC_HSE_ON;
 RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
 RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
 RCC_OscInitStruct.PLL.PLLM = 4;
-RCC_OscInitStruct.PLL.PLLN = 129;
+RCC_OscInitStruct.PLL.PLLN = 155;
 RCC_OscInitStruct.PLL.PLLP = 2;
 RCC_OscInitStruct.PLL.PLLQ = 2;
 RCC_OscInitStruct.PLL.PLLR = 2;
 RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_2;
 RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE;
-RCC_OscInitStruct.PLL.PLLFRACN = 0;
+RCC_OscInitStruct.PLL.PLLFRACN = 3072;
 if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
 	{
     Error_Handler();
@@ -219,7 +222,7 @@ RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV2;
 RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV2;
 RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV2;
 
-if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
 	{
 	Error_Handler();
 	}
@@ -229,6 +232,42 @@ else /* RCC Configuration okay */
 	}
 
 } /* SystemClock_Config */
+
+static void MX_SPI2_Init(void)
+{
+
+/* SPI2 parameter configuration*/
+hspi2.Instance = SPI2;
+hspi2.Init.Mode = SPI_MODE_MASTER;
+hspi2.Init.Direction = SPI_DIRECTION_2LINES;
+hspi2.Init.DataSize = SPI_DATASIZE_8BIT;
+hspi2.Init.CLKPolarity = SPI_POLARITY_LOW;
+hspi2.Init.CLKPhase = SPI_PHASE_1EDGE;
+hspi2.Init.NSS = SPI_NSS_SOFT;
+hspi2.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
+hspi2.Init.FirstBit = SPI_FIRSTBIT_MSB;
+hspi2.Init.TIMode = SPI_TIMODE_DISABLE;
+hspi2.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+hspi2.Init.CRCPolynomial = 0x0;
+hspi2.Init.NSSPMode = SPI_NSS_PULSE_ENABLE;
+hspi2.Init.NSSPolarity = SPI_NSS_POLARITY_LOW;
+hspi2.Init.FifoThreshold = SPI_FIFO_THRESHOLD_01DATA;
+hspi2.Init.TxCRCInitializationPattern = SPI_CRC_INITIALIZATION_ALL_ZERO_PATTERN;
+hspi2.Init.RxCRCInitializationPattern = SPI_CRC_INITIALIZATION_ALL_ZERO_PATTERN;
+hspi2.Init.MasterSSIdleness = SPI_MASTER_SS_IDLENESS_00CYCLE;
+hspi2.Init.MasterInterDataIdleness = SPI_MASTER_INTERDATA_IDLENESS_00CYCLE;
+hspi2.Init.MasterReceiverAutoSusp = SPI_MASTER_RX_AUTOSUSP_DISABLE;
+hspi2.Init.MasterKeepIOState = SPI_MASTER_KEEP_IO_STATE_DISABLE;
+hspi2.Init.IOSwap = SPI_IO_SWAP_DISABLE;
+if (HAL_SPI_Init(&hspi2) != HAL_OK)
+{
+Error_Handler();
+}
+/* USER CODE BEGIN SPI2_Init 2 */
+
+/* USER CODE END SPI2_Init 2 */
+
+}
 
 /*******************************************************************************
 *                                                                              *
@@ -300,6 +339,7 @@ GPIO_InitTypeDef GPIO_InitStruct = {0};
 /* GPIO Ports Clock Enable */
 __HAL_RCC_GPIOE_CLK_ENABLE();
 __HAL_RCC_GPIOA_CLK_ENABLE();
+__HAL_RCC_GPIOB_CLK_ENABLE();
 __HAL_RCC_GPIOH_CLK_ENABLE();
 
 
@@ -357,6 +397,19 @@ GPIO_InitStruct.Mode  = GPIO_MODE_INPUT;            /* push-pull output    */
 GPIO_InitStruct.Pull  = GPIO_NOPULL;                /* no pull up resistor */
 GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;        /* Low Frequency       */
 HAL_GPIO_Init(PWR_SRC_GPIO_PORT, &GPIO_InitStruct); /* Write to registers  */
+
+
+/*--------------------------- FLASH CHIP PINS ---------------------------------*/
+
+/*Configure GPIO pin Output Level */
+HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_RESET);
+
+/*Configure GPIO pin : PB12 */
+GPIO_InitStruct.Pin = GPIO_PIN_12;
+GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+GPIO_InitStruct.Pull = GPIO_NOPULL;
+GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 } /* GPIO_Init */
 
