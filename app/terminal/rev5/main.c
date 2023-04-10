2efb9104b2978468fe4fc659210c2baf2018e0be
Colton Acosta <cacost12@asu.edu>
2023-04-10 16:17:11 -0700

    added rev5 macro to terminal and hotfire firmware

---
+++ app/terminal/rev5/main.c
@@ -0,0 +1,337 @@
+/*******************************************************************************
+*
+* FILE: 
+* 		main.c
+*
+* DESCRIPTION: 
+* 		Processes commands recieved from a host PC, provides fine control over 
+*       engine controller hardware resources
+*
+*******************************************************************************/
+
+
+/*------------------------------------------------------------------------------
+ Standard Includes                                                                     
+------------------------------------------------------------------------------*/
+#include <stdbool.h>
+
+
+/*------------------------------------------------------------------------------
+ Project Includes                                                                     
+------------------------------------------------------------------------------*/
+
+/* Application Layer */
+#include "main.h"
+#include "sdr_pin_defines_L0002.h"
+#include "init.h"
+#include "sdr_error.h"
+
+/* Low-level modules */
+#include "commands.h"
+#include "ignition.h"
+#include "flash.h"
+#include "led.h"
+#include "power.h"
+#include "pressure.h"
+#include "sensor.h"
+#include "temp.h"
+#include "usb.h"
+#include "valve.h"
+#include "wireless.h"
+
+
+/*------------------------------------------------------------------------------
+ MCU Peripheral Handles 
+------------------------------------------------------------------------------*/
+ADC_HandleTypeDef  hadc1;  /* Pressure transducer ADC handle */
+ADC_HandleTypeDef  hadc2;  /* Load cell ADC handle           */
+I2C_HandleTypeDef  hi2c1;  /* Thermocouple I2C handle        */
+SPI_HandleTypeDef  hspi2;  /* Flash SPI handle               */
+UART_HandleTypeDef huart1; /* USB UART handler struct        */
+UART_HandleTypeDef huart2; /* Valve controller UART handle   */
+UART_HandleTypeDef huart4; /* Wireless interface UART handle */
+
+
+/*------------------------------------------------------------------------------
+ Application entry point                                                      
+------------------------------------------------------------------------------*/
+int main
+	(
+ 	void
+	)
+{
+/*------------------------------------------------------------------------------
+ Local Variables                                                                  
+------------------------------------------------------------------------------*/
+uint8_t       command;          /* SDEC command                               */
+uint8_t       subcommand;       /* subcommand code                            */
+uint8_t       pwr_source;       /* Power source code                          */
+
+/* Flash */
+HFLASH_BUFFER flash_handle;                    /* Flash API buffer handle     */
+uint8_t       flash_buffer[ DEF_BUFFER_SIZE ]; /* Flash data buffer */
+
+/* Thermocouple */
+THERMO_CONFIG thermo_config;    /* Thermocouple configuration settings        */
+
+/* Module return codes */
+FLASH_STATUS  flash_status;     /* Status of flash operations                 */
+IGN_STATUS    ign_status;       /* Ignition status code                       */
+THERMO_STATUS thermo_status;    /* Thermocouple status code                   */
+USB_STATUS    usb_status;       /* Status of USB operations                   */
+VALVE_STATUS  valve_status;     /* Valve API return codes                     */
+
+
+/*------------------------------------------------------------------------------
+ MCU Initialization                                                                  
+------------------------------------------------------------------------------*/
+HAL_Init                (); /* Reset peripherals, initialize flash interface 
+                               and Systick.                                   */
+SystemClock_Config      (); /* System clock                                   */
+PeriphCommonClock_Config(); /* Shared peripheral clocks                       */
+GPIO_Init               (); /* GPIO                                           */
+USB_UART_Init           (); /* USB UART                                       */
+Valve_UART_Init         (); /* Valve control UART                             */
+Wireless_UART_Init      (); /* Wireless interface UART                        */
+FLASH_SPI_Init          (); /* Flash SPI Bus                                  */
+PRESSURE_ADC_Init       (); /* Pressure transducers ADC                       */
+LOADCELL_ADC_Init       (); /* Load Cell ADC                                  */
+Thermocouple_I2C_Init   (); /* Thermocouple I2C                               */
+
+
+/*------------------------------------------------------------------------------
+ Variable Initializations 
+------------------------------------------------------------------------------*/
+
+/* Flash Buffer */
+flash_handle.write_protected       = FLASH_WP_WRITE_ENABLED;
+flash_handle.num_bytes             = 0;
+flash_handle.address               = 0;
+flash_handle.pbuffer               = &flash_buffer[0];
+flash_handle.status_register       = 0xFF;
+flash_handle.bpl_bits              = FLASH_BPL_NONE;
+flash_handle.bpl_write_protect     = FLASH_BPL_READ_WRITE;
+
+/* Thermocouple configuration */
+thermo_config.type                 = THERMO_TYPE_K;
+thermo_config.filter_coeff         = THERMO_FILTER_OFF; 
+thermo_config.adc_resolution       = THERMO_18BIT_ADC;
+thermo_config.cold_junc_resolution = THERMO_COLD_JUNC_MIN_RES;
+thermo_config.burst_mode           = THERMO_BURST_MODE_1;
+thermo_config.shutdown_mode        = THERMO_NORMAL_MODE;
+thermo_config.status               = 0;
+
+/* Module return codes */
+flash_status                       = FLASH_OK;
+thermo_status                      = THERMO_OK;
+usb_status                         = USB_OK;
+valve_status                       = VALVE_OK;
+
+
+/*------------------------------------------------------------------------------
+ External Hardware Initializations 
+------------------------------------------------------------------------------*/
+
+/* Flash Chip */
+flash_status = flash_init( &flash_handle );
+if ( flash_status != FLASH_OK )
+	{
+	Error_Handler( ERROR_FLASH_INIT_ERROR );
+	}
+
+/* Sensor module */
+sensor_init();
+
+/* Thermocouple */
+thermo_status = temp_init( &thermo_config );
+if ( thermo_status != THERMO_OK )
+	{
+	Error_Handler( ERROR_THERMO_INIT_ERROR );
+	}
+
+/* Indicate Successful MCU and Peripheral Hardware Setup */
+led_set_color( LED_GREEN );
+
+
+/*------------------------------------------------------------------------------
+ Event Loop                                                                  
+------------------------------------------------------------------------------*/
+while (1)
+	{
+	/* Read data from UART reciever */
+    usb_status = usb_receive( &command, sizeof( command ), HAL_DEFAULT_TIMEOUT );
+
+	/* Parse command input if HAL_UART_Receive doesn't timeout */
+	if ( usb_status != USB_TIMEOUT )
+		{
+		switch( command )
+			{
+			/*-----------------------------------------------------------------
+			 PING Command	
+			------------------------------------------------------------------*/
+			case PING_OP:
+				{
+				ping();
+				break;
+				} /* PING_OP */
+
+			/*-----------------------------------------------------------------
+			 CONNECT Command	
+			------------------------------------------------------------------*/
+			case CONNECT_OP:
+				{
+				ping();
+				break;
+				} /* CONNECT_OP */
+
+			/*-----------------------------------------------------------------
+			 IGNITE Command	
+			------------------------------------------------------------------*/
+			case IGNITE_OP:
+				{
+                /* Recieve ignition subcommand over USB */
+                usb_status = usb_receive( &subcommand         , 
+                                          sizeof( subcommand ), 
+                                          HAL_DEFAULT_TIMEOUT );
+
+                /* Execute subcommand */
+                if ( usb_status != USB_TIMEOUT )
+					{
+					/* Execute subcommand*/
+                    ign_status = ign_cmd_execute( subcommand );
+                    }
+				else
+					{
+                    /* Error: no subcommand recieved */
+                    Error_Handler( ERROR_IGN_CMD_ERROR );
+                    }
+
+                /* Return response code to terminal */
+                usb_transmit( &ign_status         , 
+				              sizeof( ign_status ), 
+							  HAL_DEFAULT_TIMEOUT );
+				break;
+				} /* IGNITE_OP */
+
+			/*-----------------------------------------------------------------
+			 POWER Command	
+			------------------------------------------------------------------*/
+			case POWER_OP:
+				{
+                /* Determine power source */
+				pwr_source = pwr_get_source();
+
+				/* Convert to response code and transmit to PC */
+                pwr_source += 1;
+				usb_transmit( &pwr_source         , 
+				              sizeof( pwr_source ), 
+							  HAL_DEFAULT_TIMEOUT );
+				break;
+				} /* POWER_OP */
+
+			/*-----------------------------------------------------------------
+			 FLASH Command	
+			------------------------------------------------------------------*/
+			case FLASH_OP:
+				{
+                /* Recieve flash subcommand over USB */
+                usb_status = usb_receive( &subcommand         , 
+                                          sizeof( subcommand ), 
+                                          HAL_DEFAULT_TIMEOUT );
+			
+				/* Execute subcommand */
+				if ( usb_status == USB_OK )
+					{
+				    flash_status = flash_cmd_execute( subcommand   , 
+                                                     &flash_handle );
+					}
+				else
+					{
+					/* Subcommand code not recieved */
+					Error_Handler( ERROR_FLASH_CMD_ERROR );
+					}
+
+				/* Transmit status code to PC */
+				usb_transmit( &flash_status, 
+                              sizeof( flash_status ), 
+                              HAL_DEFAULT_TIMEOUT );
+				break;
+				} /* FLASH_OP */
+
+			/*-----------------------------------------------------------------
+			 SENSOR Command	
+			------------------------------------------------------------------*/
+			case SENSOR_OP:
+				{
+				/* Recieve subcommand from USB */
+                usb_status = usb_receive( &subcommand         ,
+                                          sizeof( subcommand ),
+                                          HAL_DEFAULT_TIMEOUT );
+
+				/* Execute subcommand */
+				if ( usb_status != USB_OK )
+					{
+                    /* Subcommand not recieved */
+                    Error_Handler( ERROR_SENSOR_CMD_ERROR );
+                    }
+				else
+					{
+                    sensor_cmd_execute( subcommand ); 
+                    }
+
+				break;
+				} /* SENSOR_OP */
+
+			/*-----------------------------------------------------------------
+			 VALVE Command	
+			------------------------------------------------------------------*/
+			case VALVE_OP:
+				{
+				/* Get subcommand */
+				usb_status = usb_receive( &subcommand         ,
+				                          sizeof( subcommand ),
+										  HAL_DEFAULT_TIMEOUT );
+				if ( usb_status != USB_OK )
+					{
+					Error_Handler( ERROR_VALVE_CMD_ERROR );
+					}
+				
+				/* Pass on command and subcommand to valve controller */
+				valve_status = valve_transmit( &command         , 
+				                               sizeof( command ), 
+											   HAL_DEFAULT_TIMEOUT );
+				if ( valve_status != VALVE_OK )
+					{
+					Error_Handler( ERROR_VALVE_CMD_ERROR );
+					}
+				valve_status = valve_transmit( &command         , 
+				                               sizeof( command ),
+											   HAL_DEFAULT_TIMEOUT );
+				if ( valve_status != VALVE_OK )
+					{
+					Error_Handler( ERROR_VALVE_CMD_ERROR );
+					}
+				break;
+				} /* VALVE_OP */
+
+			/*-----------------------------------------------------------------
+			 Unrecognized Command 
+			------------------------------------------------------------------*/
+			default:
+				{
+				/* Unsupported command code flash the red LED */
+				//Error_Handler();
+				} /* default */
+			} 
+		} 
+	else /* USB connection times out */
+		{
+		/* Do nothing */
+		}
+	}
+} /* main */
+
+
+/*******************************************************************************
+* END OF FILE                                                                  *
+*******************************************************************************/
---
+++ app/terminal/rev5/main.h
@@ -0,0 +1,49 @@
+/*******************************************************************************
+*
+* FILE: 
+* 		main.h
+*
+* DESCRIPTION: 
+* 		Processes commands recieved from a host PC, provides fine control over 
+*       engine controller hardware resources
+*
+*******************************************************************************/
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*------------------------------------------------------------------------------
+ Includes                                                                    
+------------------------------------------------------------------------------*/
+#include "stm32h7xx_hal.h"
+
+
+/*------------------------------------------------------------------------------
+ Macros  
+------------------------------------------------------------------------------*/
+
+/* General MCU HAL related macros */
+#define DEF_BUFFER_SIZE        ( 16 )   /* Default size of buffer arrays      */
+
+/* Timeouts */
+#ifndef SDR_DEBUG
+	#define HAL_DEFAULT_TIMEOUT    ( 10  ) /* Default timeout for polling 
+											   operations                     */
+	#define HAL_SENSOR_TIMEOUT     ( 40  ) /* Timeout for sensor polling      */
+#else
+	/* Disable timeouts when debugging */
+	#define HAL_DEFAULT_TIMEOUT    ( 0xFFFFFFFF )  
+	#define HAL_SENSOR_TIMEOUT     ( 0xFFFFFFFF ) 
+#endif /* SDR_DEBUG */
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
