620881345d864e777cfd15e73dd9cb19e66b1845
Colton Acosta <cacost12@asu.edu>
2023-04-23 16:43:23 -0700

    begain fill and chill implementation

--- app/hotfire/rev5/fill_chill.c
+++ app/hotfire/rev5/fill_chill.c
@@ -12,12 +12,21 @@
 /*------------------------------------------------------------------------------
  Standard Includes                                                              
 ------------------------------------------------------------------------------*/
+#include <string.h>
 
 
 /*------------------------------------------------------------------------------
  Project Includes                                                              
 ------------------------------------------------------------------------------*/
 #include "main.h"
+#include "sensor.h"
+#include "valve_control.h"
+
+
+/*------------------------------------------------------------------------------
+ Global Variables 
+------------------------------------------------------------------------------*/
+extern FSM_STATE fsm_state; /* Hotfire State */
 
 
 /*------------------------------------------------------------------------------
@@ -39,6 +48,59 @@ FSM_STATE run_fill_chill_state
     void
     )
 {
+/*------------------------------------------------------------------------------
+ Local Variables  
+------------------------------------------------------------------------------*/
+float       lox_tank_press;  /* LOX tank pressure,  psi */
+float       fuel_tank_press; /* Fuel tank pressure, psi */
+SENSOR_DATA sensor_data;     /* Data from engine sensors */
+
+
+/*------------------------------------------------------------------------------
+ Initializations 
+------------------------------------------------------------------------------*/
+lox_tank_press  = 0;
+fuel_tank_press = 0;
+memset( &sensor_data, 0, sizeof( SENSOR_DATA ) );
+
+
+/*------------------------------------------------------------------------------
+ Implementation 
+------------------------------------------------------------------------------*/
+
+/* Stop the engine purge */
+vc_close_solenoids( SOLENOID_LOX_PRESS | SOLENOID_FUEL_PRESS );
+vc_close_main_valves( MAIN_VALVE_BOTH_MAINS );
+vc_open_solenoids( SOLENOID_LOX_VENT | SOLENOID_FUEL_VENT );
+
+/* Check Tank Pressures */
+sensor_dump( &sensor_data );
+lox_tank_press  = sensor_conv_pressure( sensor_data.pt_pressures[ PT_LOX_PRESS_INDEX ], 
+                                         PT_LOX_PRESS_INDEX );
+fuel_tank_press = sensor_conv_pressure( sensor_data.pt_pressures[ PT_FUEL_PRESS_INDEX ], 
+                                         PT_FUEL_PRESS_INDEX );
+if ( ( lox_tank_press > 600 ) || ( fuel_tank_press > 600 ) )
+    {
+    // TODO: Send Error Indication to ground station 
+    /* Enter manual control mode */
+    return FSM_MANUAL_STATE;
+    }
+
+/* Fill the tanks */
+while ( fsm_state != FSM_STANDBY_STATE )
+    {
+    sensor_dump( &sensor_data );
+    lox_tank_press  = sensor_conv_pressure( sensor_data.pt_pressures[ PT_LOX_PRESS_INDEX ], 
+                                            PT_LOX_PRESS_INDEX );
+    fuel_tank_press = sensor_conv_pressure( sensor_data.pt_pressures[ PT_FUEL_PRESS_INDEX ], 
+                                            PT_FUEL_PRESS_INDEX );
+    if ( ( lox_tank_press > 600 ) || ( fuel_tank_press > 600 ) )
+        {
+        // TODO: Send Warning Indication to ground station 
+        }
+    }
+
+/* Enter Standby state */
 return FSM_STANDBY_STATE;
 } /* run_fill_chill_state */
 
